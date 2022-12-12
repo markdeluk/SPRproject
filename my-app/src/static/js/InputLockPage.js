0ffb64d7d3a37bfd9301535173cf9d99e89f4d14
mattiaottoborgo <mattiaottoborgo@gmail.com>
2022-12-12 15:26:25 +0100

    Refactored InputLock.html

--- my-app/src/static/js/InputLockPage.js
+++ my-app/src/static/js/InputLockPage.js
@@ -1,5 +1,22 @@
 var NextButton = document.getElementById('NextButton');
 var PinLockInput = document.getElementById('PinLock');
+PinLockInput.addEventListener('keypress', function(event){
+    //Prevent the value to be added
+    event.preventDefault();
+    //Regex that you can change for whatever you allow in the input (here any word character --> alphanumeric & underscore)
+    var reg = /\w/g;
+    //retreive the input's value length
+    var inputChar = String.fromCharCode(event.which);
+    //retreive the input's value length
+    var inputLength = PinLockInput.value.length;
+    if ( reg.test(inputChar) && (inputLength < 4) ) {
+        //if input length < 4, add the value
+        PinLockInput.value = PinLockInput.value + inputChar;
+    }else{
+        //else do nothing
+        return;
+    }
+});
 NextButton.addEventListener('click',function(){InsertBikePicPage()});
 
 function InsertBikePicPage(){
